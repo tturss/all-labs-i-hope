917d2efb736361f740a4bd2df629cdbecd8d1fac
tturss <t_zhurkabayeva@kbtu.kz>
2024-09-19 12:19:18 +0500

    Add files via upload

---
+++ lab2/ifelse.py
@@ -0,0 +1,67 @@
+a = 50
+b = 10
+if a > b:
+  print("Hello World")
+
+
+  
+a = 50
+b = 10
+if a != b:
+  print("Hello World")
+
+
+
+a = 50
+b = 10
+if a == b:
+  print("Yes")
+else:
+  print("No")
+
+
+
+a = 50
+b = 10
+if a == b:
+  print("1")
+elif a > b:
+  print("2")
+else:
+  print("3")
+
+
+a=1
+b=2
+c=3
+d=4
+if a == b and c == d:
+  print("Hello")
+
+a=1
+b=1
+c=1
+d=1
+if a == b or c == d:
+  print("Hello")
+
+
+
+if 5 > 2:
+    print("YES")
+
+
+
+a = 2
+b = 5
+print("YES") if a == b else  print("NO")
+
+
+
+
+
+a = 2
+b = 50
+c = 2
+if a == c or b == c:
+  print("YES")
---
+++ lab2/lists.py
@@ -0,0 +1,39 @@
+fruits = ["apple", "banana", "cherry"]
+print(fruits[1])
+# Output: banana
+
+
+fruits = ["apple", "banana", "cherry"]
+fruits[0] = "kiwi"
+# Replace the first element with "kiwi"
+
+
+
+fruits = ["apple", "banana", "cherry"]
+fruits.append("orange")
+# Add "orange" to the end of the list
+
+
+fruits = ["apple", "banana", "cherry"]
+fruits.insert(1, "lemon")
+# Insert "lemon" at index 1
+
+
+fruits = ["apple", "banana", "cherry"]
+fruits.remove("banana")
+# Remove "banana" from the list
+
+
+fruits = ["apple", "banana", "cherry"]
+print(fruits[-1])
+# Output: ['cherry', 'orange', 'kiwi']
+
+fruits = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(fruits[2:5])
+# Output: ['cherry', 'orange', 'kiwi']
+
+
+
+fruits = ["apple", "banana", "cherry"]
+print(len(fruits))
+#3
